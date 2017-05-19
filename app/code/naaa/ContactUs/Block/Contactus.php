baafe8704b07b5820c82a8346eddb76fa04d03c8
Nazar Korchevsky <naaa.nazar@gmail.com>
2017-05-19 13:39:14 +0300

    create project

---
+++ app/code/naaa/ContactUs/Block/Contactus.php
@@ -0,0 +1,17 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: naaanazar
+ * Date: 13.05.2017
+ * Time: 14:56
+ */
+
+namespace naaa\ContactUs\Block;
+
+class Contactus extends \Magento\Framework\View\Element\Template
+{
+    public function getHelloWorldTxt()
+    {
+        return 'Contacts';
+    }
+}
---
+++ app/code/naaa/ContactUs/registration.php
@@ -0,0 +1,12 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: naaanazar
+ * Date: 13.05.2017
+ * Time: 14:45
+ */
+\Magento\Framework\Component\ComponentRegistrar::register(
+    \Magento\Framework\Component\ComponentRegistrar::MODULE,
+    'Contact',
+    __DIR__
+);
